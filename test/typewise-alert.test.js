e7bf4fc9da95284485dfac5a85753b5f36ae4ddd
srikant21 <srikant21.k.sahoo@gmail.com>
2022-07-13 16:07:51 +0530

    Add files via upload

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -1,7 +1,17 @@
 const alerts = require('../typewise-alert');
 const {expect} = require('chai');
+const sinon = require('sinon');
 
 describe('typewise alerts', function() {
+  beforeEach(function() {
+    if (null == this.sinon) {
+      this.sinon = sinon.createSandbox();
+    } else {
+      this.sinon.restore();
+    }
+    this.sinon.stub(console, 'log');
+  });
+
   it('infers a value lower than the minimum as TOO_LOW', () => {
     expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
   });
